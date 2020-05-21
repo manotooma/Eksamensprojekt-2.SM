eac62b1fdff3864bd81d5b48abdb20cd1ef5c61c
Willeboebaggins <david214@live.dk>
2020-05-21 19:56:52 +0200

    Externe java virker

--- js/script.js
+++ js/script.js
@@ -1,25 +1,10 @@
-/*testalert("hello")
-console.log("alert")*/
-
-<!--Initialize Swiper-- >
-<
-script >
-    var swiper = new Swiper('.swiper-container', {
-        pagination: {
-            el: '.swiper-pagination',
-            dynamicBullets: true,
-        },
-    });
-
-
-/* Question-----------------------------------------------------------------------------*/
-
-function pickrandomimg() {
-    const imagearray = new Array("img/polygon1.png", "img/polygon2.png", "img/polygon3.png");
-
-    let randomimagesrc = imagearray[Math.floor(Math.random() * imagearray.length)];
-
-    document.getElementById("mypicture").src = randomimagesrc;
-
-    console.log(randomimagesrc);
-}
+alert("Hello!")
+ function pickrandomimg() {
+            const imagearray = new Array("img/polygon1.png", "img/polygon2.png", "img/polygon3.png");
+            
+            let randomimagesrc = imagearray[Math.floor(Math.random() * imagearray.length)];
+            
+            document.getElementById("mypicture").src = randomimagesrc;
+            
+            console.log(randomimagesrc);
+        }

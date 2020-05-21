30b20f5c66b8d59f92846bd5f864ba81c4d2ef95
Willeboebaggins <david214@live.dk>
2020-05-21 15:05:12 +0200

    Gameboard update

--- js/script.js
+++ js/script.js
@@ -13,3 +13,9 @@ console.log("alert")*/
 
 
 /* Question-----------------------------------------------------------------------------*/
+   function pickrandomimg() {
+            const imagearray = new Array("img/polygon1.png", "img/polygon2.png", "img/polygon3.png");
+            let randomimagesrc = imagearray[Math.floor(Math.random() * imagearray.length)];
+            document.getElementById("mypicture").src = randomimagesrc;
+            console.log(randomimagesrc);
+        }

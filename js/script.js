e14e28326e8b57b664a151dcef9d9b406924b56b
Malanen <60384258+Malanen@users.noreply.github.com>
2020-05-27 15:45:27 +0200

    avatar og andet changes

--- js/script.js
+++ js/script.js
@@ -128,3 +128,25 @@ function goToWinner() {
 
 
 }
+
+
+/* Winner slider   --------------------*/
+
+
+$(document).ready(function(){
+/* Initialize Swiper when document ready --- */
+
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }}),
+    });
+  
